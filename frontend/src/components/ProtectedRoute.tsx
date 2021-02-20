e53d6f3da9870704e976911910d98b2de847b8fc
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-20 20:40:18 +0100

    Added protectedRoute component

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import Axios from 'axios'
+
+const ProtectedRoute = ({component, ...rest}: any)=> {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(false);
+    useEffect( () => {
+        Axios({
+            method: "GET",
+            withCredentials: true,
+            url: "http://localhost:8000/api/auth/user"
+          }).then(res => {
+              setUser(res.data.user);
+              setLoading(false);
+            //   console.log(user);
+          }).catch(err => console.log(err));
+    }, [])
+    
+    const RouteComponent = (props: any) => (
+        React.createElement(component, props)
+    );
+    
+    return (
+    <Route {...rest} render={ props => {
+        if (loading) {
+            <p>Loading...</p>
+        }
+        else {
+            if (user) {
+                return <RouteComponent {...props} user={user}/>
+            }
+            else {
+                return <Redirect to={ {
+                    pathname: '/auth',
+                    state: {
+                        from: props.location
+                    }
+                }}/>
+            }
+        }
+
+    }}/>
+    )
+};
+
+export default ProtectedRoute
