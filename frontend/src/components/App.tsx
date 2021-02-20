e5374e206297294e5cf04105ef5ec5f78c1a5fe7
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-20 20:11:05 +0100

    Added react router and HomePage component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../css/App.css';
 import AuthPage from './AuthPage'
+import HomePage from './HomePage'
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <AuthPage/>
+      <BrowserRouter>
+        <Route path='/auth' component={AuthPage}/>
+        <Route path='/' exact component={HomePage}/>
+      </BrowserRouter>
     </div>
   );
 }
