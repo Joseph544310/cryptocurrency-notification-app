9f810987c7be7384f09e625e112042990997675b
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-21 15:07:15 +0100

    Updated protectedroute to connect to redux store

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Axios from 'axios'
+import {connect} from 'react-redux'
 
-const ProtectedRoute = ({component, ...rest}: any)=> {
-
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState(false);
-    useEffect( () => {
-        console.log('Protected route checking user')
-        Axios({
-            method: "GET",
-            withCredentials: true,
-            url: "http://localhost:8000/api/auth/user"
-          }).then(res => {
-              setUser(res.data.user);
-              setLoading(false);
-            //   console.log(user);
-          }).catch(err => console.log(err));
-    }, [])
+const ProtectedRoute = ({component, auth, ...rest}: any)=> {
     
     const RouteComponent = (props: any) => (
         React.createElement(component, props)
@@ -25,12 +10,12 @@ const ProtectedRoute = ({component, ...rest}: any)=> {
     
     return (
     <Route {...rest} render={ props => {
-        if (loading) {
+        if (auth.isLoading) {
             <p>Loading...</p>
         }
         else {
-            if (user) {
-                return <RouteComponent {...props} user={user}/>
+            if (auth.user) {
+                return <RouteComponent {...props}/>
             }
             else {
                 console.log('not authenticated... redirecting to login')
@@ -47,4 +32,8 @@ const ProtectedRoute = ({component, ...rest}: any)=> {
     )
 };
 
-export default ProtectedRoute
+const mapStateToProps = (state: any) => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps)(ProtectedRoute)
