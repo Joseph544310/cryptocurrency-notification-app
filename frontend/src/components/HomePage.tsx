f6087cb4f3686c460d86cf0ae19ea8fd821dfe62
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-22 18:16:40 +0100

    Added header

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import CreateAlertForm from './CreateAlertForm'
 import AlertsTable from './AlertsTable'
-import {Row, Col, Button} from 'react-bootstrap'
+import {Row, Col, Button, Jumbotron} from 'react-bootstrap'
 import {FcPlus} from 'react-icons/fc'
 import {ImCancelCircle} from 'react-icons/im'
 import UserNav from './UserNav'
@@ -11,6 +11,14 @@ const HomePage = (props: any) => {
     return (
         <div>
             <UserNav/>
+            <div className='Header'>
+                <h1>Notifications Center</h1>
+                <div>
+                    <h4>Create alerts to get notified about the price fluctuations</h4>
+                    <p>Prices will be automatically checked once per day and you will receive an email when your criterias are met.</p>
+                </div>
+            </div>
+            
             {showForm?<CreateAlertForm/>:null}
             {showForm?
             <ImCancelCircle className='cross-icon' onClick={e=>setShowForm(false)}/>:
