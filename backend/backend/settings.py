efb3986c2bbf66e0cb4283fb36ec7017adcb37f5
Joseph Sabbagh <joseph544310@gmail.com>
2021-02-22 23:06:44 +0100

    Added localhost:8000 to cors allowed origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
 ]
 CORS_EXPOSE_HEADERS = ['Content-Type']
 CORS_ALLOW_CREDENTIALS = True
